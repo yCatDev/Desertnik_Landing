8445111ee368b70c159b0586ce0192d133dd0b87
Grebenuk Denis <32875166+yCatDev@users.noreply.github.com>
2020-03-23 17:18:54 +0300

    Mobile check

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
-var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-if (isMobile) {
+if( !(navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)))
     particlesJS.load('particles', 'particles.json');
-}
+
+
 
 
 var popup, upbtn;
