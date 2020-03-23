aa626262cb53f74569e62cf671e1e24b928433aa
Grebenuk Denis <32875166+yCatDev@users.noreply.github.com>
2020-03-23 20:29:08 +0300

    Animations

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 
-if(navigator.userAgent.match('Mobile')==null){
-    particlesJS.load('particles', 'particles.json'); }
-//console.log(navigator.userAgent.match('Mobile'));
+
+if (navigator.userAgent.match('Mobile') == null) {
+    particlesJS.load('particles', 'particles.json');
+}
 
 
 
 var popup, upbtn;
 window.onload = function () {
-    popup =  document.getElementById("popup1");
+    popup = document.getElementById("popup1");
     upbtn = document.getElementById("up_button");
+
 };
 
 window.addEventListener("scroll", function (event) {
     var scroll = this.scrollY;
-    if (scroll>500)
+    if (scroll > 500)
         upbtn.className = "show";
     else
         upbtn.className = "";
@@ -22,6 +24,7 @@ window.addEventListener("scroll", function (event) {
 function ShowPopup() {
     popup.style.display = 'block';
 }
+
 function HidePopup() {
     popup.style.display = 'none';
 }
@@ -41,25 +44,26 @@ var EPPZScrollTo =
         /**
          * Helpers.
          */
-        documentVerticalScrollPosition: function()
-        {
+        documentVerticalScrollPosition: function () {
             if (self.pageYOffset) return self.pageYOffset; // Firefox, Chrome, Opera, Safari.
             if (document.documentElement && document.documentElement.scrollTop) return document.documentElement.scrollTop; // Internet Explorer 6 (standards mode).
             if (document.body.scrollTop) return document.body.scrollTop; // Internet Explorer 6, 7 and 8.
             return 0; // None of the above.
         },
 
-        viewportHeight: function()
-        { return (document.compatMode === "CSS1Compat") ? document.documentElement.clientHeight : document.body.clientHeight; },
+        viewportHeight: function () {
+            return (document.compatMode === "CSS1Compat") ? document.documentElement.clientHeight : document.body.clientHeight;
+        },
 
-        documentHeight: function()
-        { return (document.height !== undefined) ? document.height : document.body.offsetHeight; },
+        documentHeight: function () {
+            return (document.height !== undefined) ? document.height : document.body.offsetHeight;
+        },
 
-        documentMaximumScrollPosition: function()
-        { return this.documentHeight() - this.viewportHeight(); },
+        documentMaximumScrollPosition: function () {
+            return this.documentHeight() - this.viewportHeight();
+        },
 
-        elementVerticalClientPositionById: function(id)
-        {
+        elementVerticalClientPositionById: function (id) {
             var element = document.getElementById(id);
             var rectangle = element.getBoundingClientRect();
             return rectangle.top;
@@ -68,16 +72,14 @@ var EPPZScrollTo =
         /**
          * Animation tick.
          */
-        scrollVerticalTickToPosition: function(currentPosition, targetPosition)
-        {
+        scrollVerticalTickToPosition: function (currentPosition, targetPosition) {
             var filter = 0.2;
             var fps = 60;
             var difference = parseFloat(targetPosition) - parseFloat(currentPosition);
 
             // Snap, then stop if arrived.
             var arrived = (Math.abs(difference) <= 0.5);
-            if (arrived)
-            {
+            if (arrived) {
                 // Apply target.
                 scrollTo(0.0, targetPosition);
                 return;
@@ -90,7 +92,7 @@ var EPPZScrollTo =
             scrollTo(0.0, Math.round(currentPosition));
 
             // Schedule next tick.
-            setTimeout("EPPZScrollTo.scrollVerticalTickToPosition("+currentPosition+", "+targetPosition+")", (1000 / fps));
+            setTimeout("EPPZScrollTo.scrollVerticalTickToPosition(" + currentPosition + ", " + targetPosition + ")", (1000 / fps));
         },
 
         /**
@@ -99,12 +101,10 @@ var EPPZScrollTo =
          * @param id The id of the element to scroll to.
          * @param padding Top padding to apply above element.
          */
-        scrollVerticalToElementById: function(id, padding)
-        {
+        scrollVerticalToElementById: function (id, padding) {
             var element = document.getElementById(id);
-            if (element == null)
-            {
-                console.warn('Cannot find element with id \''+id+'\'.');
+            if (element == null) {
+                console.warn('Cannot find element with id \'' + id + '\'.');
                 return;
             }
 
