912fcdac6554052978b947e39c95c2600ae5b1c5
Grebenuk Denis <32875166+yCatDev@users.noreply.github.com>
2020-03-23 17:22:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
-if((navigator.userAgent.match(/Android/i)
+if( !(navigator.userAgent.match(/Android/i)
     || navigator.userAgent.match(/webOS/i)
     || navigator.userAgent.match(/iPhone/i)
     || navigator.userAgent.match(/iPad/i)
     || navigator.userAgent.match(/iPod/i)
     || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)))
-    particlesJS.load('particles', 'particles.json');
-
+    || navigator.userAgent.match(/Windows Phone/i))){
+    particlesJS.load('particles', 'particles.json'); }
+alert(navigator.userAgent);
 
 
 
