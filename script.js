e5300639c241b62d950181195433f5df7050b3da
Grebenuk Denis <32875166+yCatDev@users.noreply.github.com>
2020-04-18 04:33:52 +0300

    Added favicon and fixed mobile maps appearing

--- script.js
+++ script.js
@@ -176,9 +176,7 @@ function initPage() {
 
     function OpenMap(){
         // If it's an iPhone..
-        if( (navigator.platform.indexOf("iPhone") != -1)
-            || (navigator.platform.indexOf("iPod") != -1)
-            || (navigator.platform.indexOf("iPad") != -1))
+        if(navigator.userAgent.match('Mobile') != null)
             window.open("maps://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=[YOUR_LAT],[YOUR_LNG]");
         else
         {
